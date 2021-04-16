a537250f45f2a4ecb525435b705a22fa2d0e393f
unknown <nesad.omeragich27@gmail.com>
2021-04-16 03:42:28 +0200

    Added helper function for showing date as time elapsed

---
+++ src/util/helpers.js
@@ -0,0 +1,30 @@
+export const parseDate = (date, [locale, options]) => {
+  return new Date(date).toLocaleDateString(locale, options);
+};
+
+export const timeSince = (date) => {
+  let seconds = Math.floor((new Date() - date) / 1000);
+
+  let interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + ' years';
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + ' months';
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + ' days ago';
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + ' hours ago';
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + ' minutes ago';
+  }
+  return Math.floor(seconds) + ' seconds ago';
+};
