343cf0c10f1e8edfaf2629383a588627107056b2
nesherson <nesad.omeragich27@gmail.com>
2021-12-15 19:58:14 +0100

    Fix routing issues that occured after updaing react router

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { Homepage } from '../components/Homepage/Homepage';
 import { ResultsPage } from '../components/ResultsPage/ResultsPage';
 
 function App() {
-  const location = useLocation();
-
   return (
-    <Routes location={location}>
-      <Route path='/*' element={<Homepage />}>
-        
-      </Route>
-      <Route path='/temp/:id'>
-        <ResultsPage />
-      </Route>
+    <Routes>
+      <Route path=':id' element={<ResultsPage />} />
+      <Route path='/' element={<Homepage />} />
     </Routes>
   );
 }
 
 export default App;
+
