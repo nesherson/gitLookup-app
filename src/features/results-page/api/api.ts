c2f304dc2c5aa60696c15b6f56090bd9fd371b77
nesherson <nesad.omeragich27@gmail.com>
2022-01-03 23:50:51 +0100

    Refactor data fetching logic

--- src/features/results-page/api/api.ts
+++ src/features/results-page/api/api.ts
@@ -3,15 +3,30 @@ import { User, Repo, Activity } from '../types';
 const URL = 'https://api.github.com/users/';
 const MAX_REPOS = 100;
 
-export const GetUser = async (username: string): Promise<User> => {
-  return await (await fetch(`${URL}${username}`)).json();
+const get = async (url: string): Promise<Response> => {
+  const response = await fetch(url);
+
+  if (!response.ok)
+    throw new Error(response.statusText);
+  
+  return response;
 }
 
-export const GetRepos = async (username: string): Promise<Repo> => {
-  return await (await fetch(`${URL}${username}/repos?per_page=${MAX_REPOS}`)).json();
+export const getUser = async (username: string): Promise<User> => {
+  const response = await get(`${URL}${username}`);
+
+  return response.json();
 }
 
-export const GetActivities = async (username: string): Promise<Activity> => {
-  return await (await fetch(`${URL}${username}/events`)).json();
+export const getRepos = async (username: string): Promise<Repo> => {
+  const response = await get(`${URL}${username}/repos?per_page=${MAX_REPOS}`);
+
+  return response.json();
+}
+
+export const getActivities = async (username: string): Promise<Activity[]> => {
+  const response = await fetch(`${URL}${username}/events`);
+
+  return response.json();
 }
 
