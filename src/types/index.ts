ef1cd67e1d964053bd1de9db69199768778f58d4
nesherson <nesad.omeragich27@gmail.com>
2022-12-07 23:55:56 +0100

    Add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,42 +1,67 @@
 export type User = {
-    name: string,
-    avatar_url: string,
-    html_url: string,
-    blog: string,
-    followers: number
-    following: number
-    created_at: string,
-    updated_at: string,
-    location: string,
-    repo_count: number,
-    fetchedAt: string
-}
+  name: string;
+  avatar_url: string;
+  html_url: string;
+  blog: string;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+  location: string;
+  repo_count: number;
+  fetchedAt: string;
+};
 
 export type UserJsonResponse = {
-    data?: User,
-    errors?: Array<{ message: string }>
-}
+  data?: User;
+  errors?: Array<{ message: string }>;
+};
 
 export type Repo = {
-    language: any;
-    stargazers_count: number;
-}
+  language: any;
+  stargazers_count: number;
+};
 
 export type Activity = {
-    id: number,
-    author: string,
-    type: string,
-    name: string,
-    repo: string,
-    payload: Payload,
-    created_at: string,
-}
+  id: number;
+  author: string;
+  type: string;
+  name: string;
+  repo: string;
+  payload: Payload;
+  created_at: string;
+};
 
-export type Payload =  {
-    comment: any,
-    issue: any,
-    size: number,
-    ref_type: string,
-    ref: string,
-    pull_request: any,
-  }
+export type Payload = {
+  comment: any;
+  issue: any;
+  size: number;
+  ref_type: string;
+  ref: string;
+  pull_request: any;
+};
+
+export type RelativeTimeFormatUnit =
+  | "year"
+  | "years"
+  | "quarter"
+  | "quarters"
+  | "month"
+  | "months"
+  | "week"
+  | "weeks"
+  | "day"
+  | "days"
+  | "hour"
+  | "hours"
+  | "minute"
+  | "minutes"
+  | "second"
+  | "seconds";
+
+export type Interval = {
+  ge: number;
+  divisor: number;
+  unit?: RelativeTimeFormatUnit;
+  text?: string;
+};
